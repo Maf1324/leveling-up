9fe3423e8c0d59079b652bb45e253ae9307f03a4
Matheus F <ma_ferreira21@hotmail.com>
2022-05-13 08:56:08 -0300

    First commit

---
+++ 1_madlibs/madlibs.py
@@ -0,0 +1,18 @@
+# TODO create new madlibs, each a new dict
+
+text = "Last month, I went to Disney World with {}.\nWe traveled for {} hours by {}. Finally, we arrived and it was very {}.\nThere were {} people {} everywhere.\nThere were also people dressed up in {} costumes."
+
+
+madlib = {
+    'title': 'My Trip To Disney World!',
+    'needed_words': {
+        'friend name': '',
+        'number': 0,
+        'vehicle': '',
+        'first adjective': '',
+        'second adjective': '',
+        ' "-ing" verb': '',
+        'animal': '',
+    },
+    'text': text
+}
---
+++ 1_madlibs/utils.py
@@ -0,0 +1,8 @@
+#utils
+def formated_print(msg):
+    print(msg, end="\n\n")
+
+def formated_input(msg):
+    value = input(msg)
+    print()
+    return value
