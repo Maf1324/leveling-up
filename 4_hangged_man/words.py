995aebf675b717bcab5ac5ddf668c080f59baec2
Matheus F <ma_ferreira21@hotmail.com>
2022-05-18 18:26:41 -0300

    ERROR-FIX ignored E501

--- 4_hangged_man/words.py
+++ 4_hangged_man/words.py
@@ -1,6 +1,6 @@
 from random import choice
 
-string = "Hunched Afogar Assistente Copo Intrometido Percentagem Movimento Emboscada Loiro Asilo"
+string = "Hunched Afogar Assistente Copo Intrometido Percentagem Movimento Emboscada Loiro Asilo"  # noqa E501
 words = string.split(" ")
 
 
