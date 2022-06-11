6b8d12352e0b08284dd4459b91a2dc8aca4fb762
Matheus F <ma_ferreira21@hotmail.com>
2022-06-10 23:02:54 -0300

    FIX flake8 problems

--- 1_madlibs/madlibs.py
+++ 1_madlibs/madlibs.py
@@ -1,6 +1,6 @@
 # TODO create new madlibs, each a new dict
 
-text = "Last month, I went to Disney World with {}.\nWe traveled for {} hours by {}. Finally, we arrived and it was very {}.\nThere were {} people {} everywhere.\nThere were also people dressed up in {} costumes."
+text = "Last month, I went to Disney World with {}.\nWe traveled for {} hours by {}. Finally, we arrived and it was very {}.\nThere were {} people {} everywhere.\nThere were also people dressed up in {} costumes."  # noqa: E501
 
 
 madlib = {
--- 1_madlibs/utils.py
+++ 1_madlibs/utils.py
@@ -1,6 +1,7 @@
-#utils
+# utils
 def formated_print(msg):
     print(msg, end="\n\n")
+
 
 def formated_input(msg):
     value = input(msg)
